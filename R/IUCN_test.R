f5dfa05258aa058ec69748f153c095b64cf82368
juliettesouny <juliette.souny@gmail.com>
2021-02-17 10:25:14 +0100

    :v: Test model function

---
+++ R/IUCN_test.R
@@ -0,0 +1,83 @@
+#This function tests the IUCN model by running 10 models on each your downsampled splitted data
+#Entry : 
+#The output from the data_prep function
+#Output : a list with
+# - rel_inf : The variable importance of each of your traits
+# - preds : Three metrics to evaluate your model, Accuracy, Kappa and TSS
+# - Balanced_accuracy : How well your models performs across categories
+
+IUCN_test = function(data_split){
+  
+  ranger_loop = mclapply(1:length(data_split),function(i){
+    
+    mclapply(1:10,function(p){
+      
+      #SHuffling data, then Splitting into training and test data
+      split <- initial_split(data_split[[i]][sample(nrow(data_split[[i]])),], prop = 0.8)
+      
+      train <- training(split)
+      test <- testing(split)
+      
+      #Creating the model and predicting to test data
+      mod = ranger(IUCN ~ ., data = train , probability = F,
+                   importance ="permutation",num.trees = 1000,mtry = 3)
+      
+      score=predict(mod,data=test)
+      
+      CM=confusionMatrix(score$predictions, test$IUCN)
+      
+      model_output = list(data.frame(variable_importance <- rownames_to_column(as.data.frame(importance(mod)))),
+                          data.frame(Accuracy = CM$overall[1],
+                                     Kappa = CM$overall[2],
+                                     TSS = CM$byClass[1]+CM$byClass[2]-1),
+                          Balanced_Accuracy = CM$byClass[11])
+      
+    })
+    
+  })
+  
+  #Getting variable importance from output list
+  rel_inf = ranger_loop %>%
+    #Transfomring list into large dataframe
+    flatten_df() %>%
+    #Recuperating column names
+    unnest(cols=c())%>%
+    #Keeping only variables and their importance
+    dplyr::select(rowname,importance.mod.)%>%
+    na.omit()%>%
+    #Getting mean variable importance over all iterations
+    group_by(rowname)%>%
+    dplyr::summarize(importance.mod.=mean(importance.mod.))
+  
+  
+  #Getting model performance from output list
+  preds = ranger_loop %>%
+    #Transfomring list into large dataframe
+    flatten_df() %>%
+    #Recuperating column names
+    unnest(cols=c())%>%
+    rename(Balanced_Accuracy="Balanced Accuracy")%>%
+    #Keeping only variables and their importance
+    dplyr::select(Accuracy:TSS)%>%
+    na.omit()%>%
+    map_dbl(mean)%>%
+    as_tibble(rownames=NA)%>%
+    rownames_to_column("Metric")
+  
+
+  #Getting balanced accuracy from output list
+  preds_class = ranger_loop %>%
+    #Transfomring list into large dataframe
+    flatten_df() %>%
+    #Recuperating column names
+    unnest(cols=c())%>%
+    rename(Balanced_Accuracy="Balanced Accuracy")%>%
+    #Keeping only variables and their importance
+    dplyr::select(Balanced_Accuracy)%>%
+    na.omit()%>%
+    dplyr::summarize(Balanced_Accuracy=mean(Balanced_Accuracy))
+  
+  output = list(rel_inf,preds,preds_class)
+
+
+}
--- R/missForest.R
+++ R/missForest.R
@@ -1,32 +1,3 @@
-#Loading packages
-pkgs <- c("tidyverse","missForest","gbm","tidymodels","caret","visdat","analytics","mice","VIM","Amelia","Hmisc","mi","parallel")
-nip <- pkgs[!(pkgs %in% installed.packages())]
-nip <- lapply(nip, install.packages, dependencies = TRUE)
-ip   <- unlist(lapply(pkgs, require, character.only = TRUE, quietly = TRUE))
-
-#Loading necessary data
-load(file=file.path(data_dir,"FB_vars.Rdata"))
-
-#Preparing data to impute. We delete Genus and Family because too many categories to predict
-lelz = FB_vars %>%
-  dplyr::select(-c(IUCN,Genus,Family,Env_1))%>%
-  head(500)
-
-#This function tests the missForest algorithm on your data without the NAs in order to see which variables we can estimate
-#Entry :
-#- A dataframe with species as rownames, traits as columns and NO NA
-#Output : 
-# A dataframe with, for each traits, a R² or mean according to type of traits giving missforest performance
-
-FB_imp = FB_vars %>%
-  #We don't use the IUCN values because that's what we want to predict
-  dplyr::select(-c(IUCN,Env_1,Genus,Family))%>%
-  na.omit()
-
-#Producing 20% of NA in dataset
-FB_imp_na = FB_imp %>%
-  prodNA(0.2)
-
 missForest_test = function(data_noNA){
   
   #Imputing 20% of NA in complete data 
@@ -90,7 +61,6 @@ missForest_test = function(data_noNA){
   
 }
 
-test = missForest_test(FB_imp)
 
 #This function fills out missing data for variables for which the missforest performed well enough
 #Entry : 
