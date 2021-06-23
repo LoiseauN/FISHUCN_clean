eedb01ef74409a26d35827ffdc91eec36e5fb6d0
Nicolas Loiseau <nloiseau@iMac-de-Nicolas.local>
2021-06-23 16:21:45 +0200

    :fish: data for zonation ready

--- R/data_for_zonation.R
+++ R/data_for_zonation.R
@@ -8,8 +8,19 @@
 #' @export
 #' 
 #' 
-#------------------Loading outputs----------------------
+#' 
+#------------------Rescale Function----------------------
 
+#Function to rescale proba
+rescalex <- function(a,b,data){
+  step1 <- data-min(data,na.rm=T)
+  step2 <- b-a
+  step3 <- max(data,na.rm=T)-min(data,na.rm=T)
+  step4 <- (step1*step2)/step3
+  res <- step4 + a
+  return(res)}
+
+#------------------Loading outputs----------------------
 path = (here::here("outputs"))
 setwd(path)
 files <- list.files(here::here("outputs"))
@@ -21,7 +32,7 @@ pred_deep <- subset(pred_deep,pred_deep$proba>=75)
 
 #Data from machine
 load("res_inference_machine.RData")
-pred_mach <- subset(res_inference_machine,res_inference_machine$percentage>=75)
+pred_mach <- subset(res_inference_machine,res_inference_machine$percentage>=80)
 
 #Merge Data
 FISHUCN <- merge(pred_deep,pred_mach,by="species",all=T)
@@ -123,8 +134,34 @@ else if(data_zonation$IUCN_cat[i] =="CR") data_zonation$IUCN_alone[i] <- "Thr"
 data_zonation <- merge(data_zonation,FISHUCN,  by = "species", all.x = T)
 data_zonation$proba_select <- as.numeric(data_zonation$proba_select)/100
 
-#keep only predicted and species already in IUCN 
+# Remove three line problem
+data_zonation <- data_zonation[!data_zonation$species %in% c("1102","152","194"),]
 
+
+
+  rescale_threat <-  subset(data_zonation, data_zonation$predict =="Thr" & is.na(data_zonation$IUCN_alone))
+  rescale_non_threat <-  subset(data_zonation, data_zonation$predict =="NThr" & is.na(data_zonation$IUCN_alone))
+  rescale_threat$proba_rescale <-  rescalex(a=2,b=5,data=rescale_threat$proba_select)
+  rescale_non_threat$proba_rescale <-  rescalex(a=1,b=2,data=1-rescale_non_threat$proba_select)
+  
+  rescale_data <- data.frame(species = c(rescale_threat$species ,
+                                         rescale_non_threat$species),
+                            proba_rescale = c(rescale_threat$proba_rescale ,
+                                              rescale_non_threat$proba_rescale))
+  
+
+  data_zonation <- merge(data_zonation,rescale_data, by = "species", all.x = T)
+
+  
+#--- Prepare for scenario  
+  
+  
+  # SCENARIO 1: 
+  # 1 for everyone
+  data_zonation$scenario1_NoWeight <- 1
+  data_zonation$scenario2_IUCNalone <- NA
+  data_zonation$scenario3_IUCN_and_Predict <- NA
+  
 data_zonation$selected_species <- NA
 data_zonation$selected_species_consensus <- NA
 
@@ -140,100 +177,102 @@ for(i in 1:nrow(data_zonation)){
   
 }
   
+
+
 data_zonation_list <- list()
-  data_zonation_list[[1]] <- subset(data_zonation,data_zonation$selected_species == 1)[,-c(13:14)]
-  data_zonation_list[[2]] <- subset(data_zonation,data_zonation$selected_species_consensus == 1)[,-c(13:14)]
-  data_zonation_list[[3]] <- data_zonation[,-c(13:14)]
+#keep only predicted and species already in IUCN 
+  data_zonation_list[[1]] <- subset(data_zonation,data_zonation$selected_species == 1)[,-c(11,17:18)]
+  #keep only predicted and species already in IUCN with consensus
+  data_zonation_list[[2]] <- subset(data_zonation,data_zonation$selected_species_consensus == 1)[,-c(10,17:18)]
+  colnames(data_zonation_list[[2]])[10] <- "predict"
+   #all species
+  data_zonation_list[[3]] <- data_zonation[,-c(11,17:18)]
   
 names(data_zonation_list) <- c("main", "sensibility1","sensibility2")
   
-  
-#Function to rescale proba
-rescalex <- function(a,b,data){
-  step1 <- data-min(data,na.rm=T)
-  step2 <- b-a
-  step3 <- max(data,na.rm=T)-min(data,na.rm=T)
-  step4 <- (step1*step2)/step3
-  res <- step4 + a
-  return(res)}
-
-# SCENARIO 1: 
-# 1 for everyone
-for (i in length())
-data_zonation$scenario1 <- 1 
-#Rescale proba for scenario
-
-
-rescale_threat <-  subset(data_zonation, data_zonation$IUCN_last_pred =="Threatened"& is.na(data_zonation$IUCN_alone))
-rescale_non_threat <-  subset(data_zonation, data_zonation$IUCN_last_pred =="NonThreatened"& is.na(data_zonation$IUCN_alone))
-
-rescale_threat$C <-  rescalex(a=2,b=5,data=rescale_threat$C)
-rescale_non_threat$NC <-  rescalex(a=1,b=2,data=1-rescale_non_threat$NC)
 
 
 # SCENARIO 3  :  Weighted in function of the machine learning probability with negative influence for non threaten
-# NA  by IUCN and predit = 1.5 (for SM 2 and 1)
-# NonThreatened by IUCN = 1
+# NA  by IUCN and predit = 2 (for SM 2 and 1)
+# NonThreatened by IUCN = 1 or if both model AGREE
 # NonThreatened predit = 2-probability
-# Threatened by IUCN = 5
+# Threatened by IUCN = 6
 # Threatened predit = 2 + probability (range between 2 and 5)
-data_zonation$scenario1_NoWeight <- 1
-data_zonation$scenario2_IUCNalone <- NA
-data_zonation$scenario3_IUCN_and_Predic <- NA
+# or 5 if both model AGREE
 
-for(i in 1:nrow(data_zonation)){
-  print(i)
+
+data_final_zonation <- lapply(1: length(data_zonation_list), function(x){
   
-  #Non information at all
-  if(is.na(data_zonation$IUCN_last_pred[i])){
-    
-    data_zonation$scenario2_IUCNalone[i] <- 1
-    data_zonation$scenario3_IUCN_and_Predic[i] <- 1 # ou 1.5 
-  }
+  data <- data_zonation_list[[x]]
   
-  if(data_zonation$IUCN_last_pred[i] %in% "NonThreatened"){
+  for(i in 1:nrow(data)){
+   
+    print(i)
     
-    #Non Threatened predict par model
-    if(is.na(data_zonation$IUCN_alone[i])){ 
-      
-      data_zonation$scenario2_IUCNalone[i] <- 1
-      data_zonation$scenario3_IUCN_and_Predic[i] <- rescale_non_threat[rescale_non_threat$species %in% data_zonation$species[i],]$NC
-      
-    }else{
-      data_zonation$scenario2_IUCNalone[i] <- 1
-      data_zonation$scenario3_IUCN_and_Predic[i] <- 1
-      
+    if(data$IUCN_alone[i] %in% "Thr"){
+      data$scenario2_IUCNalone[i] <- 6
+      data$scenario3_IUCN_and_Predict[i] <-  6 }
+    
+     else if (data$IUCN_alone[i] %in% "NThr"){
+      data$scenario2_IUCNalone[i] <- 1
+      data$scenario3_IUCN_and_Predict[i] <- 1
     }
-  }
-  
-  if(data_zonation$IUCN_last_pred[i] %in% "Threatened"){
+
+    else{ 
+    #Non information at all
+    if(is.na(data$predict[i])){
+      
+      data$scenario2_IUCNalone[i] <- 2
+      data$scenario3_IUCN_and_Predict[i] <- 2 # ou 1.5 
     
-    if(is.na(data_zonation$IUCN_alone[i])){ 
-      
-      data_zonation$scenario2_IUCNalone[i] <- 1
-      data_zonation$scenario3_IUCN_and_Predic[i] <- rescale_threat[rescale_threat$species %in% data_zonation$species[i],]$C
-      
-      
-    }else{
-      
-      data_zonation$scenario2_IUCNalone[i] <- 5
-      data_zonation$scenario3_IUCN_and_Predic[i] <-  5
-      
     }
+    
+      else if(data$predict[i] %in% "NThr"){
+      
+      #Non Threatened predict par model
+      if (!data$agree[i]  %in% "AGREE"){ 
+        data$scenario2_IUCNalone[i] <- 1
+        data$scenario3_IUCN_and_Predict[i] <- data[data$species %in% data$species[i],]$proba_rescale
+        
+      }
+      else { 
+          
+          data$scenario2_IUCNalone[i] <- 1
+          data$scenario3_IUCN_and_Predict[i] <- 1
+       }
+    }
+    
+      else if(data$predict[i] %in% "Thr"){
+      
+      if (!data$agree[i]  %in% "AGREE"){ 
+      
+        data$scenario2_IUCNalone[i] <- 1
+        data$scenario3_IUCN_and_Predict[i] <- data[data$species %in% data$species[i],]$proba_rescale
+      
+        } 
+      
+      else { 
+          
+          data$scenario2_IUCNalone[i] <- 1
+          data$scenario3_IUCN_and_Predict[i] <- 5
+          
+        }
+    }
+    
   }
+  }  
+  return(data)
+})
   
-}
+names(data_final_zonation) <- c("main", "sensibility1","sensibility2")
+  boxplot(data_final_zonation[[1]]$scenario1_NoWeight,
+          data_final_zonation[[1]]$scenario2_IUCNalone,
+          data_final_zonation[[1]]$scenario3_IUCN_and_Predict)
 
+  
 
-data_zonation_reduced <- data_zonation[,c("species",
-                                          "scenario1_NoWeight",
-                                          "scenario2_IUCNalone",
-                                          "scenario3_IUCN_and_Predic")]
+save(data_final_zonation,file= here::here("outputs","data_final_zonation.RData"))
 
-
-save(data_zonation_reduced,file= "data_zonation_reduced.RData")
-#Reduce for Laure 
-load("data_zonation.RData")
 
 
 
