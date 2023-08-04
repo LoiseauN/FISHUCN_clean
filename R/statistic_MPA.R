174d3b06e5502b9cbbc84408d00acb96e94c5281
nloiseau <nicolas.loiseau@umontpellier.fr>
2023-08-04 18:06:15 +0200

    push stat for conservation

---
+++ R/statistic_MPA.R
@@ -0,0 +1,111 @@
+
+library(ggplot2)
+library(magrittr)
+load(here::here("MPA_Protect.RData"))
+
+# --- Stat tests: --------------------------------------------------------------
+# 1. Test if there is a difference between before and after within each category
+# The data are not normally distributed so I used a Wilcoxon test. And in fact
+# the data aren't paired because the number of samples change between before and 
+# after (it's what we want to test). A paired test would be comparing measures
+# that would change between before and after...
+
+# 2. Test if there is a difference between categories TH/NS/NT within each 
+# before/after group. I used a Kruskal Wallis test because there are more than 2 
+# groups.
+
+
+BEFORE <- cbind.data.frame(perc_cover=MPA_Protect$perc_cover, Target_achievement_I_IV=MPA_Protect$Target_achievement_I_IV,IUCN=MPA_Protect$IUCN_cat,What="BEFORE")
+AFTER <- cbind.data.frame(perc_cover=MPA_Protect$perc_cover, Target_achievement_I_IV=MPA_Protect$Target_achievement_I_IV,IUCN=MPA_Protect$IUCN_final,What="AFTER")
+MPA_FINAL <- rbind(BEFORE,AFTER)
+
+MPA_FINAL$What <- factor(MPA_FINAL$What, levels = c("AFTER","BEFORE"))
+MPA_FINAL$IUCN <- factor(MPA_FINAL$IUCN, levels = c("Threatened","Non Threatened","No Status"))
+MPA_FINAL$category <- as.factor(paste(MPA_FINAL$IUCN, MPA_FINAL$What, sep="_"))
+MPA_FINAL$category <- factor(MPA_FINAL$category,levels=c("No Status_BEFORE","No Status_AFTER","Non Threatened_BEFORE","Non Threatened_AFTER","Threatened_BEFORE","Threatened_AFTER")) 
+
+
+
+options(digits = 3, scipen = 5)
+
+## TARGET ACHIEVEMENT
+
+# Before - after 
+NS <- wilcox.test(MPA_FINAL$Target_achievement_I_IV[MPA_FINAL$category == "No Status_BEFORE"],
+                  MPA_FINAL$Target_achievement_I_IV[MPA_FINAL$category == "No Status_AFTER"])
+NS
+Threat <- wilcox.test(MPA_FINAL$Target_achievement_I_IV[MPA_FINAL$category == "Threatened_BEFORE"],
+                      MPA_FINAL$Target_achievement_I_IV[MPA_FINAL$category == "Threatened_AFTER"])
+Threat
+NT <- wilcox.test(MPA_FINAL$Target_achievement_I_IV[MPA_FINAL$category == "Non Threatened_BEFORE"],
+                  MPA_FINAL$Target_achievement_I_IV[MPA_FINAL$category == "Non Threatened_AFTER"])
+NT
+
+# Intra group:
+m_before <- kruskal.test(BEFORE$Target_achievement_I_IV ~ BEFORE$IUCN)
+dt_before <- FSA::dunnTest(BEFORE$Target_achievement_I_IV~BEFORE$IUCN)
+dt_before$res
+
+
+m_after <- kruskal.test(AFTER$Target_achievement_I_IV ~ AFTER$IUCN)
+dt_after <- FSA::dunnTest(AFTER$Target_achievement_I_IV~AFTER$IUCN)
+dt_after$res
+
+## PERCENT COVER
+
+# Before - after 
+NS <- wilcox.test(MPA_FINAL$perc_cover[MPA_FINAL$category == "No Status_BEFORE"],
+                  MPA_FINAL$perc_cover[MPA_FINAL$category == "No Status_AFTER"])
+NS
+Threat <- wilcox.test(MPA_FINAL$perc_cover[MPA_FINAL$category == "Threatened_BEFORE"],
+                      MPA_FINAL$perc_cover[MPA_FINAL$category == "Threatened_AFTER"])
+Threat
+NT <- wilcox.test(MPA_FINAL$perc_cover[MPA_FINAL$category == "Non Threatened_BEFORE"],
+                  MPA_FINAL$perc_cover[MPA_FINAL$category == "Non Threatened_AFTER"])
+NT
+
+# Intra group:
+m_before <- kruskal.test(BEFORE$perc_cover ~ BEFORE$IUCN)
+dt_before <- FSA::dunnTest(BEFORE$perc_cover~BEFORE$IUCN)
+dt_before$res
+
+
+m_after <- kruskal.test(AFTER$perc_cover ~ AFTER$IUCN)
+dt_after <- FSA::dunnTest(AFTER$perc_cover~AFTER$IUCN)
+dt_after$res
+
+
+# sample sizes
+table(MPA_FINAL$category)
+
+# --- Stat tests end -----------------------------------------------------------
+
+
+# --- Figure -------------------------------------------------------------------
+
+
+
+violin_targets <- cowplot::ggdraw() + cowplot::draw_image(here::here("violin_targets_ggsave.png"))
+violin_cover <- cowplot::ggdraw() + cowplot::draw_image(here::here("violin_cover_ggsave.png"))
+target_fig <- cowplot::ggdraw() + cowplot::draw_image(here::here("targets.png"))
+cover_fig <- cowplot::ggdraw() + cowplot::draw_image(here::here("perc cover.png"))
+
+
+png("fig_all.png", 
+    width = 24, 
+    height = 15.75, 
+    units = "in", 
+    res = 600,
+    pointsize = 18)
+
+fig1 <- gridExtra::grid.arrange(
+  violin_targets, violin_cover, target_fig, cover_fig,
+  nrow=2)
+
+dev.off()
+
+# --- Figure end ---------------------------------------------------------------
+
+
+
+
