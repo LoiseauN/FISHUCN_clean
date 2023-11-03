e99e5bfdceafd117f847e849b267431711133728
nloiseau <nicolas.loiseau@umontpellier.fr>
2023-11-03 16:38:45 +0100

    legend

--- analysis/Maps_data.R
+++ analysis/Maps_data.R
@@ -316,61 +316,54 @@ if (var[x] =="richness_finalNT")  {ggsave(file = here::here("figures/IUCN_FinalN
   
 #'--------------------------------------------------------@DeltaRankzonation
  if  (var[x] =="DeltaRank_SameWeight" || var[x] == "DeltaRank_Proba"|| var[x] == "cate_DeltaRank_SameWeight")  {  
-
-# adjustcolor( "chartreuse4", alpha.f = 0.7)
-   colormap = c(rev(colorRampPalette(brewer.pal(n = 8, name = "Blues"))(334)),'white',colorRampPalette(brewer.pal(n = 8, name = "Oranges"))(164))
-
+   mask.full.polygon.plot <- mask.full.polygon %>% 
+     mutate(mask.full = Hmisc::cut2(mask.full,g=500), include.lowest = T)
    
-map <- ggplot(world) +
-  geom_sf(data = mask.full.polygon, aes(fill = mask.full, color = mask.full))+ #aes(fill = scale(mask.full), color = scale(mask.full))) +
-
-  
-  scale_colour_gradientn(na.value = "#66CDAA66",name  = "Rank after - rank before",
-                         colours = colormap)+
-  
-  scale_fill_gradientn(na.value = "#66CDAA66",name  = "Rank after - rank before",
-                       colours = colormap)+
-
-#scale_colour_gradientn(na.value = "#66CDAA66", name  = "Rank after - rank before",
-#                        colours = colorRampPalette(rev(brewer.pal(n = 8, name = "RdBu")))(100))+ #,
-#limits = c(min(c(all_geo_res$DeltaRank_SameWeight,all_geo_res$DeltaRank_Proba),na.rm = T), 
-#max(c(all_geo_res$DeltaRank_SameWeight,all_geo_res$DeltaRank_Proba),na.rm = T))) +                              
-  
-#  scale_fill_gradientn(na.value = "#66CDAA66",name  = "Rank after - rank before",
-#                       colours = colorRampPalette(rev(brewer.pal(n = 8, name = "RdBu")))(100))+ #,
-  #limits = c(min(c(all_geo_res$DeltaRank_SameWeight,all_geo_res$DeltaRank_Proba),na.rm = T), 
-  #                                    max(c(all_geo_res$DeltaRank_SameWeight,all_geo_res$DeltaRank_Proba),na.rm = T))) +
- 
-#scale_fill_gradient2(midpoint= 0, low="#084594", mid="white", name  = "Delta in priority",
-#                     high="#8C2D04", space ="Lab",na.value = "#7CCD7C80")+
-  #scale_color_gradient2(midpoint= 0, low="#084594", mid="white",name  = "Delta in priority",#00AFBB
-#                   high="#8C2D04", space ="Lab",na.value = "#7CCD7C80")+
-  
-  geom_sf(data = world, fill = "white", color = "#bebebe", size = 0.1) +
-  geom_graticules(mol) +
-  geom_mapframe(mol, colour = "white", size = 2.0) +
-  #geom_mapframe(mol, colour = "black", size = 0.4) +
-  
-  ylab(" ") +
-  xlab(" ") +
-  
-  #ggthemes::theme_map(base_family = "serif") +
-  theme_bw()+
-  theme(legend.position = "bottom",#c(0.85, 0.1),
-        legend.direction = "horizontal",
-        legend.title    =  element_text(face = "plain", size = 32,vjust = 1,hjust = 0.5),
-        legend.text     = element_text(face = "plain", size = 32),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        #legend.key.height= unit(1.5, 'cm'),
-        legend.key.width= unit(4, 'cm'),
-        axis.text=element_text(size=18),
-        axis.title=element_text(size=20),
-        legend.box="horizontal") +
-  guides(colour = guide_colourbar(title.position="top", title.hjust = 0.5),
-         size = guide_legend(title.position="top", title.hjust = 0.5))
-
+   colormap = c(rev(colorRampPalette(brewer.pal(n = 8, name = "Blues"))(334)),'white',colorRampPalette(brewer.pal(n = 8, name = "Oranges"))(164))
+   
+   map <- ggplot(world) +
+     geom_sf(data = mask.full.polygon.plot, aes(fill = mask.full, color = mask.full))+ #aes(fill = scale(mask.full), color = scale(mask.full))) +
+     
+     scale_colour_manual(na.value = "#66CDAA66",name  = "Rank after - rank before",
+                         values = colormap)+
+     
+     scale_fill_manual(na.value = "#66CDAA66",name  = "Rank after - rank before",
+                       values = colormap)+
+     
+     geom_sf(data = world, fill = "white", color = "#bebebe", size = 0.1) +
+     geom_graticules(mol) +
+     geom_mapframe(mol, colour = "white", size = 2.0) +
+     #geom_mapframe(mol, colour = "black", size = 0.4) +
+     
+     ylab(" ") +
+     xlab(" ") +
+     
+     #ggthemes::theme_map(base_family = "serif") +
+     theme_bw()+
+     theme(legend.position = "none",#bottom",#c(0.85, 0.1),
+           #legend.direction = "horizontal",
+           #legend.title    =  element_text(face = "plain", size = 32,vjust = 1,hjust = 0.5),
+           #legend.text     = element_text(face = "plain", size = 32),
+           panel.grid.major = element_blank(),
+           panel.grid.minor = element_blank(),
+           panel.border = element_blank(),
+           #legend.key.height= unit(1.5, 'cm'),
+           #legend.key.width= unit(4, 'cm'),
+           axis.text=element_text(size=18),
+           axis.title=element_text(size=20),
+           #legend.box="horizontal"
+     ) +
+     guides(colour = guide_colourbar(title.position="top", title.hjust = 0.5),
+            size = guide_legend(title.position="top", title.hjust = 0.5))
+   
+   ggsave(file = here::here("figures/cate_Figure6b.png"),map,width = 12, height = 8, units= "in",dpi= 300)
+   
+   
+   
+ pal.bands(c(rev(colorRampPalette(brewer.pal(n = 8, name = "Blues"))(334)),'white',colorRampPalette(brewer.pal(n = 8, name = "Oranges"))(164)),
+           main = "Rank after - rank before",)
+   
+   
 if (var[x] =="DeltaRank_SameWeight")   { ggsave(file = here::here("figures/Figure6b.png"),map,width = 12, height = 8, units= "in",dpi= 300)}
 
 if (var[x] =="cate_DeltaRank_SameWeight")   { ggsave(file = here::here("figures/cate_Figure6b.png"),map,width = 12, height = 8, units= "in",dpi= 300)}
